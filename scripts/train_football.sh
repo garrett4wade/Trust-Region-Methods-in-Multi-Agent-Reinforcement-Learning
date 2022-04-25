9bf0b149b6a5e01ccaa92d5c17b231fe74630692
garrett4wade <fuwth17@gmail.com>
2022-04-25 02:22:53 -0400

    finalize football config

--- scripts/train_football.sh
+++ scripts/train_football.sh
@@ -9,7 +9,7 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is 
 for number in $(seq ${running_max}); do
     echo "the ${number}-th running:"
     CUDA_VISIBLE_DEVICES=0 python3 train/train_football.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} \
-        --n_training_threads 32 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 \
-        --ppo_epoch 5 --kl_threshold ${kl_threshold} --n_eval_rollout_threads 8 \
+        --n_rollout_threads 50 --num_mini_batch 2 --episode_length 200 --data_chunk_length 10 \
+        --ppo_epoch 15 --kl_threshold ${kl_threshold} --n_eval_rollout_threads 100 \
         --use_eval --share_reward --use_recurrent_policy --use_wandb
 done
